c0aabcc51371de018e120f01c2f3134afe2e5c58
kindpingwork <kindpingwork@gmail.com>
2023-11-28 20:17:38 +0800

    字串 數字 字串加數字 型態轉換

--- assets/js/js_basic.js
+++ assets/js/js_basic.js
@@ -1,12 +1,61 @@
 // alert('Alert from js_base.js'); 
-let name = 'David Lin'
+// let name = 'David Lin'
 
-console.log(name)
-console.log('name')
-console.table(name + 'AAAA');
+// console.log(name)
+// console.log('name')
+// console.table(name + 'AAAA');
 
-let nums = [1, 4, 6, 8, 9];
-console.log(nums);
-console.table(nums);
+// let nums = [1, 4, 6, 8, 9];
+// console.log(nums);
+// console.table(nums);
 
-console.log('%c鴨沒肉！！！\n%c我只是亂喊一下啦~', 'color:red;font-size:20px;', 'color:#00ff00')
+// console.log('%c鴨沒肉！！！\n%c我只是亂喊一下啦~', 'color:red;font-size:20px;', 'color:#00ff00')
+
+
+let firstName = 'Lin';
+let lastName = 'David';
+let fullName = firstName + ' ' + 'GGGGG ' + lastName
+
+console.log(fullName);
+
+
+let n1 = 1
+let n2 = 1.25
+let n3 = n1 + n2
+
+console.log(n1, n2, n3);
+
+var isPalindrome = function (x) {
+    if (x < 0) {
+        return false
+    }
+
+    r = x
+    y = ''
+    while (r > 0) {
+        y += r % 10
+        r = Math.floor(r / 10)
+    }
+
+    return x == y
+}
+
+let n = 1
+let s = '10'
+let ns = n + s
+
+console.log(typeof n, typeof s, typeof ns);
+
+function add(num1, num2) {
+    // num1 = parseInt(num1, 10);
+    // num2 = parseInt(num2, 10);
+    num1 = +num1;
+    num2 = +num2;
+
+    let total = num1 + num2;
+    console.log(typeof total)
+    return total;
+}
+
+console.log(add(1, 1));
+console.log(add('1', 1));
