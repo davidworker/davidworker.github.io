4e80c32238f113a440be71b810e1e4b393f102a6
kindpingwork <kindpingwork@gmail.com>
2023-11-28 19:54:33 +0800

    變數宣告與console 輸出

--- assets/js/js_basic.js
+++ assets/js/js_basic.js
@@ -1,1 +1,12 @@
-alert('Alert from js_base.js');
+// alert('Alert from js_base.js'); 
+let name = 'David Lin'
+
+console.log(name)
+console.log('name')
+console.table(name + 'AAAA');
+
+let nums = [1, 4, 6, 8, 9];
+console.log(nums);
+console.table(nums);
+
+console.log('%c鴨沒肉！！！\n%c我只是亂喊一下啦~', 'color:red;font-size:20px;', 'color:#00ff00')
