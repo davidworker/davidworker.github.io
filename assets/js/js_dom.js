defbad1f160beadcb67a055c448f75ad04faeeab
kindpingwork <kindpingwork@gmail.com>
2023-11-28 21:14:19 +0800

    事件綁定

--- assets/js/js_dom.js
+++ assets/js/js_dom.js
@@ -2,9 +2,23 @@ let uid = document.querySelector('#uid');
 let student = document.querySelector('.student');
 
 console.log(uid);
+
+uid.innerHTML = 'New UID';
+
 console.log(student);
 
 let uids = document.querySelectorAll('#uid');
 let students = document.querySelectorAll('.student');
 console.log(uids);
 console.log(students);
+
+
+let fullName = document.querySelector('#full-name');
+
+console.log(fullName);
+
+let btn = document.querySelector('#btn');
+
+btn.addEventListener('click', (e) => {
+    alert('Click Me!!!!!!!!');
+})
