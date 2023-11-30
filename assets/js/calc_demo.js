26dde63d51f2da64ad9b6b6ed33425e8112217ae
kindpingwork <kindpingwork@gmail.com>
2023-11-30 19:19:42 +0800

    四則運算基本架構

---
+++ assets/js/calc_demo.js
@@ -0,0 +1,9 @@
+let num1 = document.querySelector('#calc-num-1');
+let num2 = document.querySelector('#calc-num-2');
+let type = document.querySelector('#calc-type');
+let btn = document.querySelector('#calc-btn');
+let response = document.querySelector('#response');
+
+btn.addEventListener('click', (e) => {
+    console.log(123);
+})
