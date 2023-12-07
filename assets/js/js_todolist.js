c683c373b446b9a521d0db8bb22ceeefe6837766
kindpingwork <kindpingwork@gmail.com>
2023-12-07 21:22:12 +0800

    todolist add function

---
+++ assets/js/js_todolist.js
@@ -0,0 +1,18 @@
+let elInput = document.querySelector('#todo-in');
+let elAddBtn = document.querySelector('#todo-add-btn');
+let elItem = document.querySelector('#todo-item');
+
+elAddBtn.addEventListener('click', (e) => {
+    let value = elInput.value;
+    if (!value) {
+        elInput.focus();
+        return;
+    }
+
+    elItem.innerHTML += `<li>
+                            <div><input type="checkbox"></div>
+                            <div>${value}</div>`
+
+    elInput.value = '';
+    elInput.focus();
+})
