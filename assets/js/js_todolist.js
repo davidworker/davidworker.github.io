f325245f7b1520d9b4209e7a2e396cf3ff2f69fd
kindpingwork <kindpingwork@gmail.com>
2023-12-14 21:06:30 +0800

    POST 與 檔案上傳

--- assets/js/js_todolist.js
+++ assets/js/js_todolist.js
@@ -89,4 +89,66 @@ const doFetch = () => {
     console.log('doFetch run.')
 }
 
-doFetch();
+// doFetch();
+
+
+const doFetchPost = () => {
+    let api = 'https://book.niceinfos.com/frontend/api/'
+
+    let params = {
+        action: 'demo',
+        data: { a: 1, b: 2 },
+    }
+
+    let options = {
+        method: 'POST',
+        body: JSON.stringify(params),
+    }
+
+    fetch(api, options)
+        .then((response) => {
+            return response.text()
+            // return response.json()
+        })
+        .then((data) => {
+            console.log(data)
+        })
+}
+
+// doFetchPost();
+
+const doUploadFile = (file) => {
+    if (!file) {
+        return
+    }
+
+    let api = 'https://book.niceinfos.com/frontend/api/'
+
+    let form = new FormData()
+    form.append('action', 'upload')
+    form.append('file', file)
+
+    let options = {
+        method: 'POST',
+        body: form,
+    }
+
+    fetch(api, options)
+        .then((response) => {
+            return response.text()
+            // return response.json()
+        })
+        .then((data) => {
+            console.log(data)
+        })
+}
+
+let elUpload = {
+    file: document.querySelector('#upload-file'),
+    button: document.querySelector('#do-upload'),
+}
+
+elUpload.button.addEventListener('click', (e) => {
+    let file = elUpload.file.files[0]
+    doUploadFile(file)
+})
