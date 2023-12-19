126f2aabf9735ebfa31172f99e23f72b886e3115
kindpingwork <kindpingwork@gmail.com>
2023-12-19 21:02:52 +0800

    realtime 寫入測試

---
+++ assets/js/firebase_realtime.js
@@ -0,0 +1,7 @@
+import { App } from './Firebase/App.js'
+import { Database } from './Firebase/Database.js'
+
+const app = await App.init();
+const database = new Database(app);
+
+database.write('david', { name: 'David Lin', age: 18, items: [1, 2, 3, 4, 5] })
