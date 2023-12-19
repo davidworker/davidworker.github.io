f5eeba78b86a29c1a53b9f8a33262828f187bc58
kindpingwork <kindpingwork@gmail.com>
2023-12-19 21:30:38 +0800

    監聽,下次繼續講

--- assets/js/firebase_realtime.js
+++ assets/js/firebase_realtime.js
@@ -4,4 +4,13 @@ import { Database } from './Firebase/Database.js'
 const app = await App.init();
 const database = new Database(app);
 
-database.write('david', { name: 'David Lin', age: 18, items: [1, 2, 3, 4, 5] })
+// database.write('david', { name: 'David Lin', age: 18, items: [1, 2, 3, 4, 5] })
+
+// let response = await database.read('david');
+// console.log(response);
+
+const onChange = (data) => {
+    console.log(data);
+}
+
+database.listen('david', onChange);
